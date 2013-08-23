b3543b399f909ce0983df8ffedb170d5835ebb54
drawlabs <ryan@drawlabs.com>
2013-08-23 04:25:55 -0700

    Product config.

---
+++ Products/ProductsConfig.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class ProductsConfig {
+	
+	// IOS
+	public static string iOSSharedSecret = "8750ac5cfbdc4a40b51b899059636243";
+	
+	// PLAY
+	
+	// AMAZON
+	
+	// WINDOWS
+	
+	// WEB (AMAZON?)
+	
+}
+
