d59f7a85f16a6f4aa02a84fb23136b785486637d
drawlabs <ryan@drawlabs.com>
2015-06-14 11:46:35 -0700

    Update card, chance and deck generator.

--- Game/Gameplay/Cards/GameCards.cs
+++ Game/Gameplay/Cards/GameCards.cs
@@ -35,18 +35,225 @@ public class GameCard : GameDataObject {
 }
 
 public class GameCardDeck { 
+
+    // unique deck code
+    public string code = "default";
+
+    // deck type i.e. special cards, regular etc per game config
+    public string type = "default";
+
     public List<GameCard> cards;
+    public Queue cardQueue;
 
     public GameCardDeck() {
+
         cards = new List<GameCard>();
+        cardQueue = new Queue();
+
+        PrepareCards();
+    }
+        
+    public void SetCard(string cardType, string cardValue) {
+        GameCard gameCard = new GameCard();
+
+        gameCard.type = cardType;
+        gameCard.val = cardValue;
+
+        cards.Add(gameCard);
+    }
+
+    public void SetCard(GameCard gameCard) {
+        cards.Add(gameCard);
+    }
+    
+    public void Shuffle() {
+        cards.Shuffle();
+    }
+
+    public List<GameCard> GetCards() {
+        return cards;
+    }
+        
+    public GameCard DealCard() {
+
+        if(cardQueue.Count == 0) {
+            return null;
+        }
+
+        return (GameCard)cardQueue.Dequeue();
+    }
+
+    public void ClearCards() {
+        cards.Clear();
+    }
+    
+    public void ClearQueue() {
+        cardQueue.Clear();
+    }
+
+    public void LoadCards(List<GameCard> cardsTo) {
+        ClearCards();   
+        cards.AddRange(cardsTo);
+        PrepareCards();
+    }
+    
+    public void PrepareCards() {
+
+        ClearQueue();
+        
+        Shuffle();   
+
+        foreach(GameCard gameCard in cards) {
+            cardQueue.Enqueue(gameCard);
+        }
+
+        Debug.Log("GameCards:PrepareCards: cardCount:" + cardQueue.Count);
+
     }
 }
 
+public class GameCardsGenerator {
+        
+    static List<string> gameCardTypes;    
+    static List<string> gameCardValues;
+    
+    public static List<string> GetStandardCardTypes() {
+        
+        if(gameCardTypes == null) {
+            gameCardTypes = new List<string>();    
+            
+            gameCardTypes.Add("spade");
+            gameCardTypes.Add("heart");
+            gameCardTypes.Add("diamond");
+            gameCardTypes.Add("clover");
+        }
+        
+        return gameCardTypes;
+    }
+    
+    public static List<string> GetStandardCardValues() {
+
+        if(gameCardValues == null) {
+            gameCardValues = new List<string>();    
+        }
+
+        gameCardValues.Clear();
+
+        for(int i = 0; i < 9; i++) {
+            gameCardValues.Add((i+1).ToString());            
+        }
+        
+        gameCardValues.Add("ace");
+        gameCardValues.Add("queen");
+        gameCardValues.Add("jack");
+        gameCardValues.Add("king");
+    
+        return gameCardValues;
+    }
+
+    public static GameCardDeck GetStandardCardDeck() {
+        GameCardDeck deck = new GameCardDeck();
+
+        deck.LoadCards(GetStandardCards());
+
+        return deck;
+    }
+
+    public static List<GameCard> GetStandardCards() {
+
+        List<GameCard> cards = new List<GameCard>();
+
+        foreach(string cardType in GetStandardCardTypes()) {
+            
+            foreach(string cardVal in GetStandardCardValues()) {
+                
+                cards.Add(CreateCard(cardType, cardVal));
+            }
+        }
+
+        return cards;
+    }
+        
+    public static GameCard CreateCard(string cardType, string cardValue) {
+        GameCard gameCard = new GameCard();
+        
+        gameCard.type = cardType;
+        gameCard.val = cardValue;
+        
+        return gameCard;
+    }
+
+}
+
 public class GameCardSet {
+
     public List<GameCardDeck> decks;
+    public GameCard currentCard = null;
+
+    public Queue cardQueue;
 
     public GameCardSet() {
+
+        Reset();
+    }
+
+    public void Reset() {
+
         decks = new List<GameCardDeck>();
+        cardQueue = new Queue();
+        currentCard = null;        
+    }
+    
+    public void LoadDeck(GameCardDeck gameCardDeck) {
+
+        decks.Add(gameCardDeck);
+    }
+    
+    public void Shuffle() {
+
+        foreach(GameCardDeck deck in decks) {
+            deck.Shuffle();
+        }
+    }
+
+    public void ClearQueue() {
+
+        cardQueue.Clear();
+        
+        foreach(GameCardDeck deck in decks) {
+            deck.ClearCards();
+        }
+    }    
+
+    public void PrepareCards() {        
+
+        foreach(GameCardDeck deck in decks) {
+            deck.PrepareCards();
+        }
+
+        foreach(GameCardDeck deck in decks) {
+
+            foreach(GameCard card in deck.GetCards()) {
+                cardQueue.Enqueue(card);
+            }
+        }
+
+        currentCard = null;
+    }
+    
+    public GameCard CurrentCard() {
+        return currentCard;
+    }
+        
+    public GameCard DealCard() {
+
+        if(cardQueue.Count == 0) {
+            return null;
+        }
+
+        currentCard = (GameCard)cardQueue.Dequeue();
+
+        return currentCard;
     }
 }
 
@@ -54,6 +261,9 @@ public class GameCards {
     
     private static volatile GameCards instance;
     private static System.Object syncRoot = new System.Object();
+
+    public GameCardSet cardSet;
+    public GameCard currentCard;
     
     public static GameCards Instance {
         get {
@@ -66,8 +276,27 @@ public class GameCards {
             return instance;
         }
     }
-    
-    public GameCards() {
 
+    public GameCards() {
+        cardSet = new GameCardSet();
+        LoadCards();
+    }
+
+    public void LoadCards(string type = "default", int deckCount = 3) {
+        currentCard = null;
+        cardSet.Reset();
+        for(int i = 0; i < deckCount; i++) {
+            cardSet.LoadDeck(GameCardsGenerator.GetStandardCardDeck());
+        }
+        cardSet.PrepareCards();
+    }
+
+    public GameCard DealCard() {
+    
+        currentCard = cardSet.DealCard();
+
+        Debug.Log("GameCard:DealCard:" + currentCard.ToJson());
+
+        return currentCard;
     }
 }
---
+++ Game/Gameplay/Cards/GameCardsGameObject.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Engine.Data.Json;
+using Engine.Events;
+using Engine.Utility;
+
+public class GameCardsGameObject : GameObjectBehavior {
+
+    GameCards gameCards = GameCards.Instance;
+
+    public void Start() {
+
+    }
+
+    public void DealCard() {
+        GameCard gameCard = gameCards.DealCard();
+        
+        Debug.Log("DealCard:" + gameCard.ToJson());
+    }
+
+    public void LoadCards() {
+        gameCards.LoadCards();
+        
+        
+        Debug.Log("LoadCards:" + gameCards.cardSet.cardQueue.ToJson());
+    }
+
+    public void HandleInput() {
+        
+        if(Input.GetKeyDown(KeyCode.P)) {
+            LoadCards();
+        }
+
+        if(Input.GetKeyDown(KeyCode.U)) {
+            DealCard();
+        }
+    }
+
+    public void Update() {
+
+        HandleInput();
+    }
+
+}
