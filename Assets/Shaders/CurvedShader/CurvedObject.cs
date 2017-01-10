557a9e998be871a067e859eab061cc9bd522a249
drawcode <ryan@drawcode.com>
2017-01-09 18:34:25 -0700

    Update curved shaders for opaque and transparent. Update infinite gameplay loading and curving/bending.

---
+++ Assets/Shaders/CurvedShader/CurvedObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Attach this script to all 3D models having textures.
+/// It sets the shader value, which is responsible for bending of path.
+/// </summary>
+/// 
+public class CurvedObject : MonoBehaviour {
+
+    void Start() {
+        UpdateShader();
+    }
+
+    void Update() {
+        UpdateShader();
+    }
+
+    void UpdateShader() {
+        GetComponent<Renderer>().sharedMaterial.SetVector("_QOffset", GameController.GetCurveInfiniteAmount());
+        GetComponent<Renderer>().sharedMaterial.SetFloat("_Distance", GameController.GetCurveInfiniteDistance());
+    }
+}
