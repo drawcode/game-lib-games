d2668dd2ad76264927f48e9009c7cd5a8d49488a
drawcode <ryan@drawcode.com>
2017-01-25 22:04:23 -0700

    Update stationary/infinite controller and handling.

--- Game/Objects/GameObjectInfiniteContainer.cs
+++ Game/Objects/GameObjectInfiniteContainer.cs
@@ -219,6 +219,11 @@ public class GameObjectInfiniteContainer : GameObjectBehavior {
 
     void LoadLevelAssetDynamicByIndex(int indexItem, bool clear = false) {
 
+        // Use off screen location to spawn before move
+        Vector3 spawnLocation = Vector3.zero.WithY(5000);
+
+        bool used = false;
+
         // ADD PART ITEMS CONTAINER
 
         GameObject go = AppContentAssets.LoadAssetLevelAssets(data.codeGamePartItems);
@@ -243,6 +248,9 @@ public class GameObjectInfiniteContainer : GameObjectBehavior {
         part.index = indexItem;
         Vector3 bounds = part.bounds;
 
+        //Vector3 infinityPosition = go.transform.position.WithZ(
+        //    (((indexItem + 1) * bounds.z) - bounds.z) + distance.z);
+
         go.transform.position = go.transform.position.WithZ(
             (((indexItem + 1) * bounds.z) - bounds.z) + distance.z);
 
@@ -254,16 +262,16 @@ public class GameObjectInfiniteContainer : GameObjectBehavior {
         string template4 = "  X";
 
         for (int i = 0; i < data.lines.Count; i++) {
-
+            
             GameObject goItem = AppContentAssets.LoadAssetLevelAssets(data.codeGamePartItem);
 
             if (goItem == null) {
                 Debug.Log("Asset not found levelassets/" + data.codeGamePartItem);
                 continue;
             }
-
+            
             goItem.DestroyChildren();
-
+            
             goItem.transform.parent = go.transform;
             goItem.transform.position = go.transform.position;
             goItem.transform.localPosition = goItem.transform.localPosition.WithX(data.lines[i].x);
@@ -278,33 +286,42 @@ public class GameObjectInfiniteContainer : GameObjectBehavior {
 
             int rand = UnityEngine.Random.Range(1, 10);
 
-            if (rand < 2 && !clear) {
-                //continue;
+            bool fillBlock = false;
+
+            if (rand > 2 && !clear) {
+                fillBlock = true;
+            }
+            else if (clear) {
+                fillBlock = true;
             }
 
-            // ADD PART BLOCK AND ASSETS FROM TEMPLATE
+            if (fillBlock) {
 
-            GameObject goAssetBlock = AppContentAssets.LoadAssetLevelAssets(data.codeGameBlock);
+                // ADD PART BLOCK AND ASSETS FROM TEMPLATE
 
-            if (goAssetBlock == null) {
-                Debug.Log("Asset not found levelassets/" + data.codeGameBlock);
-                continue;
+                GameObject goAssetBlock = AppContentAssets.LoadAssetLevelAssets(data.codeGameBlock);
+
+                if (goAssetBlock == null) {
+                    Debug.Log("Asset not found levelassets/" + data.codeGameBlock);
+                    continue;
+                }
+
+                goAssetBlock.Hide();
+
+                goAssetBlock.transform.parent = goItem.transform;
+                goAssetBlock.transform.position = goItem.transform.position;
+                //goAssetBlock.transform.localPosition = goItem.transform.localPosition.WithX(data.lines[i].x);
+
+                goAssetBlock.Show();
             }
-
-            goAssetBlock.Hide();
-
-            goAssetBlock.transform.parent = goItem.transform;
-            goAssetBlock.transform.position = goItem.transform.position;
-            //goAssetBlock.transform.localPosition = goItem.transform.localPosition.WithX(data.lines[i].x);
-
-            goAssetBlock.Show();
 
             // ADD ITEMS
 
+            // ADD ITEM COIN
 
             int randCoin = UnityEngine.Random.Range(1, 10);
 
-            if (randCoin < 2 && !clear) {
+            if (randCoin < 2 && !clear && !used) {
 
                 string itemCoin = "item-coin";
 
@@ -322,12 +339,15 @@ public class GameObjectInfiniteContainer : GameObjectBehavior {
                 goAssetItem.transform.localPosition = goItem.transform.localPosition.WithX(0).WithY(2f).WithZ(0);
 
                 goAssetItem.Show();
+
+                used = true;
             }
 
+            // ADD ITEM SPECIAL
 
             int randLetter = UnityEngine.Random.Range(1, 10);
 
-            if (randLetter < 2 && !clear) {
+            if (randLetter < 2 && !clear && !used) {
 
                 string itemLetter = "item-special-watermelon";
 
@@ -345,17 +365,16 @@ public class GameObjectInfiniteContainer : GameObjectBehavior {
                 goAssetItem.transform.localPosition = goItem.transform.localPosition.WithX(0).WithY(2f).WithZ(0);
 
                 goAssetItem.Show();
+
+                used = true;
             }
-
-            //
-            continue;
-
+            
 
             // ADD PART BLOCK JUMP AND ASSETS FROM TEMPLATE
 
             int randObstacleLow = UnityEngine.Random.Range(1, 30);
 
-            if (randObstacleLow < 5 && !clear) {
+            if (randObstacleLow < 5 && !clear && !used) {
                 GameObject goAssetObstacleLow = AppContentAssets.LoadAssetLevelAssets(data.codeGameBlockLow);
 
                 if (goAssetObstacleLow == null) {
@@ -368,12 +387,19 @@ public class GameObjectInfiniteContainer : GameObjectBehavior {
                 goAssetObstacleLow.transform.parent = goItem.transform;
                 goAssetObstacleLow.transform.position = goItem.transform.position;
 
-                goAssetObstacleLow.transform.localPosition = goAssetObstacleLow.transform.localPosition.WithY(bounds.y);
+                //goAssetObstacleLow.transform.localPosition = goAssetObstacleLow.transform.localPosition.WithY(bounds.y);
 
                 goAssetObstacleLow.Show();
+
+                used = true;
             }
 
         }
+        
+        //go.transform.position = infinityPosition.WithY(0);
+
+
+        //go.ResetLocalPosition();
 
         /*
 
