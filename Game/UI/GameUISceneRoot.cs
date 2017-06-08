1ece22d282bd954897807ed7c5627fe0b57c7fd0
drawcode <ryan@drawcode.com>
2017-06-07 19:40:34 -0700

    Update initial sync for Android. Update ar data for pathing.

--- Game/UI/GameUISceneRoot.cs
+++ Game/UI/GameUISceneRoot.cs
@@ -8,6 +8,8 @@ using UnityEngine.UI;
 
 using Engine.Events;
 
+using Vuforia;
+
 public class GameUISceneRoot : GameObjectBehavior {
 
 #if USE_UI_NGUI_2_7 || USE_UI_NGUI_3
@@ -31,144 +33,164 @@ public class GameUISceneRoot : GameObjectBehavior {
     public float currentProgressItemEasing = 0.0f;
     float progressLevel = 0;
     float progressLevelCount = 0;
-    
+
     public void OnEnable() {
         Messenger<object>.AddListener(ContentMessages.ContentSyncShipContentSuccess, OnContentSyncShipContentSuccess);
-        
-        Messenger<string,string,float>.AddListener(
-                ContentMessages.ContentProgressMessage, 
-                OnContentProgressMessageHandler);        
+
+        Messenger<string, string, float>.AddListener(
+                ContentMessages.ContentProgressMessage,
+                OnContentProgressMessageHandler);
         Messenger<ContentItemStatus>.AddListener("content-item-status", OnContentItemStatus);
     }
 
     public void OnDisable() {
         Messenger<object>.RemoveListener(ContentMessages.ContentSyncShipContentSuccess, OnContentSyncShipContentSuccess);
-    
-        Messenger<string,string,float>.RemoveListener(
-                ContentMessages.ContentProgressMessage, 
-                OnContentProgressMessageHandler);        
+
+        Messenger<string, string, float>.RemoveListener(
+                ContentMessages.ContentProgressMessage,
+                OnContentProgressMessageHandler);
         Messenger<ContentItemStatus>.RemoveListener("content-item-status", OnContentItemStatus);
     }
-    
+
     public void OnContentSyncShipContentSuccess(object obj) {
+
+//#if UNITY_ANDROID
+        // Android has to be loaded dynamically after 
+        // copying /syncing files into persistence folder
+        // due to the split binary loading and finding Vuforia 
+        // files /datasets/trackers
+
+        Debug.Log("OnContentSyncShipContentSuccess");
+
+        Debug.Log("OnContentSyncShipContentSuccess::SyncContentList");
+
+        BasicContents.Instance.SyncContentList();
+
+        Debug.Log("OnContentSyncShipContentSuccess:start:DataSetLoadDynamic");
+
+        string dataSetCode = Application.persistentDataPath + "/QCAR/sudbudz-1.xml";
+
+        BasicARController.DataSetLoadDynamic(dataSetCode);
+
+        Debug.Log("OnContentSyncShipContentSuccess:end:DataSetLoadDynamic:" + dataSetCode);
+//#endif
+
         loadAsync.LoadLevel("GameSceneDynamic");
     }
-    
+
     IEnumerator Start() {
-                
+
         //var glob = GameGlobal.Instance;
 
         // HACK to prevent game starting in portrait due to Unity bug 
 
-        switch (Input.deviceOrientation) {
+        switch(Input.deviceOrientation) {
 
-        case DeviceOrientation.FaceDown:
+            case DeviceOrientation.FaceDown:
 
-        case DeviceOrientation.FaceUp:
+            case DeviceOrientation.FaceUp:
 
-        case DeviceOrientation.Portrait:
+            case DeviceOrientation.Portrait:
 
-        case DeviceOrientation.PortraitUpsideDown:
+            case DeviceOrientation.PortraitUpsideDown:
 
-            // None landscape orientation, set it manually
+                // None landscape orientation, set it manually
 
-           // Screen.orientation = ScreenOrientation.LandscapeLeft;
+                // Screen.orientation = ScreenOrientation.LandscapeLeft;
 
-            // Wait a bit
+                // Wait a bit
 
-            //yield return new WaitForSeconds(0.5f);
+                //yield return new WaitForSeconds(0.5f);
 
-            // Set back to autorotation, it should be alright by now
+                // Set back to autorotation, it should be alright by now
 
-            //Screen.orientation = ScreenOrientation.AutoRotation;
+                //Screen.orientation = ScreenOrientation.AutoRotation;
 
-            break;
+                break;
 
         }
-        
+
         //Application.LoadLevel("GameUISceneSplash");   
-        
+
         yield return null;
 
     }
-    
+
     public void OnContentItemStatus(ContentItemStatus contentItemStatus) {
         currentProgressItemEasing = (float)contentItemStatus.itemProgress;
     }
-    
+
     public void SetTitle(string title) {
         UIUtil.SetLabelValue(labelProgressTitle, title);
     }
-    
+
     public void SetDescription(string description) {
         UIUtil.SetLabelValue(labelProgressMessage, CleanupString(description));
     }
-            
+
     public void SetProgress(float progress) {
-            
+
         progress = Mathf.Clamp(progress, .1f, 1f);
-        
+
         float currentProgress = progress;
-        
+
         progressLevel = (float)Contents.Instance.displayState;
         progressLevelCount = (float)Contents.Instance.displayStateCount;
-        
+
         currentProgress = (progress + progressLevel) / progressLevelCount;
-                        
+
         //(0 + 3) / 7
         //(1 + 3) / 7
-        
+
         currentEasingProgress = currentProgress;
         currentProgressItemEasing = progress;
     }
-    
+
     public void OnContentProgressMessageHandler(
         string title, string description, float progress) {
-        
+
         SetTitle(title);
         SetDescription(description);
         SetProgress(progress);
     }
-    
+
     public string ToTitleCase(string str) {
         return System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
     }
-    
+
     public string CleanupString(string val) {
         val = val.Replace("-", " ");
         val = val.Replace("_", " ");
         val = ToTitleCase(val);
         return val;
     }
-    
-    void Update() {         
-        
-        if (currentEasingProgress > currentItemProgress) {
-                
+
+    void Update() {
+
+        if(currentEasingProgress > currentItemProgress) {
+
             //currentEasingProgress -= Time.deltaTime;
-            
+
             UIUtil.SetSliderValue(sliderProgress, currentEasingProgress);
         }
-        else if (currentEasingProgress < currentItemProgress) {
-                
+        else if(currentEasingProgress < currentItemProgress) {
+
             currentEasingProgress += Time.deltaTime;
-            
-            UIUtil.SetSliderValue(sliderProgress, currentEasingProgress);           
+
+            UIUtil.SetSliderValue(sliderProgress, currentEasingProgress);
         }
-        
-        if (currentProgressItemEasing > currentProgressItem) {
-            
+
+        if(currentProgressItemEasing > currentProgressItem) {
+
             currentProgressItemEasing -= Time.deltaTime;
-            
+
             UIUtil.SetSliderValue(sliderProgressItem, currentProgressItemEasing);
         }
-        else if (currentProgressItemEasing < currentProgressItem) {
-                
+        else if(currentProgressItemEasing < currentProgressItem) {
+
             currentProgressItemEasing += Time.deltaTime;
-            
-            UIUtil.SetSliderValue(sliderProgressItem, currentProgressItemEasing);   
+
+            UIUtil.SetSliderValue(sliderProgressItem, currentProgressItemEasing);
         }
     }
-    
 }
-
