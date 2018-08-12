f1df29caac4802691f3805307cdd7a652547507f
drawcode <ryan@drawcode.com>
2018-08-12 00:23:42 -0700

    Update formatting.

--- Game/Vehicle/AI/Scripts/Editor/AIDriverMenu.cs
+++ Game/Vehicle/AI/Scripts/Editor/AIDriverMenu.cs
@@ -4,15 +4,13 @@ using System;
 using UnityEditor;
 using System.Diagnostics;
 
-public class GameVehicleAIDriverMenu : GameObjectBehavior
-{
-    
+public class GameVehicleAIDriverMenu : GameObjectBehavior {
+
     private GameObject m_container;
     public GameObject waypointFolder;
 
     [MenuItem("GameObject/AI Driver Toolkit/AI Driver")]
-    static void CreateAIDPrototype()
-    {
+    static void CreateAIDPrototype() {
         GameObject prefab = AssetDatabase.LoadAssetAtPath("Assets/GameVehicleAIDriverToolkit/Prefabs/GameVehicleAIDriverPrototype.prefab", typeof(GameObject)) as GameObject;
         GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
         newObject.name = "AI Driver";
@@ -20,8 +18,7 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
         // positioned new object
         Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
+        if(Physics.Raycast(ray, out hit)) {
             newObject.transform.position = hit.point;
         }
 
@@ -32,8 +29,7 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
     }
 
     [MenuItem("GameObject/AI Driver Toolkit/Buggy")]
-    static void CreateAIDBuggy()
-    {
+    static void CreateAIDBuggy() {
         GameObject prefab = AssetDatabase.LoadAssetAtPath("Assets/GameVehicleAIDriverToolkit/Prefabs/AIBuggy.prefab", typeof(GameObject)) as GameObject;
         GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
         newObject.name = "Buggy";
@@ -41,8 +37,7 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
         // positioned new object
         Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
+        if(Physics.Raycast(ray, out hit)) {
             newObject.transform.position = hit.point;
         }
 
@@ -53,8 +48,7 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
     }
 
     [MenuItem("Component/AI/GameObject/AI Driver")]
-    static void CreateAIDPrototypeNew()
-    {
+    static void CreateAIDPrototypeNew() {
         GameObject prefab = AssetDatabase.LoadAssetAtPath("Assets/GameVehicleAIDriverToolkit/Prefabs/GameVehicleAIDriverPrototypeNew.prefab", typeof(GameObject)) as GameObject;
         GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
         newObject.name = "AI Driver";
@@ -62,8 +56,7 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
         // positioned new object
         Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
+        if(Physics.Raycast(ray, out hit)) {
             newObject.transform.position = hit.point;
         }
 
@@ -74,8 +67,7 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
     }
 
     [MenuItem("Component/AI/GameObject/Buggy Example")]
-    static void CreateAIDBuggyNew()
-    {
+    static void CreateAIDBuggyNew() {
         GameObject prefab = AssetDatabase.LoadAssetAtPath("Assets/GameVehicleAIDriverToolkit/Prefabs/AIBuggyNew.prefab", typeof(GameObject)) as GameObject;
         GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
         newObject.name = "Buggy Example";
@@ -83,8 +75,7 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
         // positioned new object
         Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
+        if(Physics.Raycast(ray, out hit)) {
             newObject.transform.position = hit.point;
         }
 
@@ -93,149 +84,140 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
         selectedObjects[0] = newObject;
         Selection.objects = selectedObjects;
     }
-	
-	[MenuItem("Component/AI/GameObject/Barrier")]
-    static void AddAddBarrier()
-    {
-       
+
+    [MenuItem("Component/AI/GameObject/Barrier")]
+    static void AddAddBarrier() {
+
         GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		newObject.transform.localScale = new Vector3(1, 2, 10);
+        newObject.transform.localScale = new Vector3(1, 2, 10);
         newObject.name = "Barrier";
-		
-		newObject.AddComponent<BarrierBehaviour>();
-		
+
+        newObject.AddComponent<BarrierBehaviour>();
+
         // positioned new object
-        Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));		
+        Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {			
-            newObject.transform.position = hit.point;				
+        if(Physics.Raycast(ray, out hit)) {
+            newObject.transform.position = hit.point;
         }
 
         //select new object
         UnityEngine.Object[] selectedObjects = new UnityEngine.Object[1];
         selectedObjects[0] = newObject;
         Selection.objects = selectedObjects;
-		Vector3 newPos = newObject.transform.position;
-		newPos.y = newPos.y + 1;
-		newObject.transform.position = newPos;
-		
+        Vector3 newPos = newObject.transform.position;
+        newPos.y = newPos.y + 1;
+        newObject.transform.position = newPos;
+
     }
-	
-	/// <summary>
-	/// Adds an oa mode switcher.
-	/// </summary>
-	[MenuItem("Component/AI/GameObject/OA Mode Switcher")]
-    static void AddOaModeSwitcher()
-    {
-       
+
+    /// <summary>
+    /// Adds an oa mode switcher.
+    /// </summary>
+    [MenuItem("Component/AI/GameObject/OA Mode Switcher")]
+    static void AddOaModeSwitcher() {
+
         GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		newObject.transform.localScale = new Vector3(10, 1, 1);
+        newObject.transform.localScale = new Vector3(10, 1, 1);
         newObject.name = "OA Mode Switcher";
-		
-		newObject.AddComponent<SwitchOAMode>();
-		
+
+        newObject.AddComponent<SwitchOAMode>();
+
         // positioned new object
-        Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));		
+        Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {			
-            newObject.transform.position = hit.point;				
+        if(Physics.Raycast(ray, out hit)) {
+            newObject.transform.position = hit.point;
         }
 
         //select new object
         UnityEngine.Object[] selectedObjects = new UnityEngine.Object[1];
         selectedObjects[0] = newObject;
         Selection.objects = selectedObjects;
-		Vector3 newPos = newObject.transform.position;
-		newPos.y = newPos.y + 1;
-		newObject.transform.position = newPos;
-		
+        Vector3 newPos = newObject.transform.position;
+        newPos.y = newPos.y + 1;
+        newObject.transform.position = newPos;
+
     }
-	
-	/// <summary>
-	/// Adds a waypoint set changer.
-	/// </summary>
-	[MenuItem("Component/AI/GameObject/Waypoint Set Changer")]
-    static void AddWaypointSetChanger()
-    {
-       
+
+    /// <summary>
+    /// Adds a waypoint set changer.
+    /// </summary>
+    [MenuItem("Component/AI/GameObject/Waypoint Set Changer")]
+    static void AddWaypointSetChanger() {
+
         GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		newObject.transform.localScale = new Vector3(10, 1, 1);
+        newObject.transform.localScale = new Vector3(10, 1, 1);
         newObject.name = "Waypoint Set Changer";
-		
-		newObject.AddComponent<GameAIWaypointChangeSet>();
-		
+
+        newObject.AddComponent<GameAIWaypointChangeSet>();
+
         // positioned new object
-        Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));		
+        Ray ray = SceneView.lastActiveSceneView.camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {			
-            newObject.transform.position = hit.point;				
+        if(Physics.Raycast(ray, out hit)) {
+            newObject.transform.position = hit.point;
         }
 
         //select new object
         UnityEngine.Object[] selectedObjects = new UnityEngine.Object[1];
         selectedObjects[0] = newObject;
         Selection.objects = selectedObjects;
-		Vector3 newPos = newObject.transform.position;
-		newPos.y = newPos.y + 1;
-		newObject.transform.position = newPos;
-		
+        Vector3 newPos = newObject.transform.position;
+        newPos.y = newPos.y + 1;
+        newObject.transform.position = newPos;
+
     }
-	
+
     [MenuItem("Component/AI/Component/AI Controller")]
-    static void AddAIController()
-    {
+    static void AddAIController() {
         AddAIControllerItems();
     }
-    
-    static void AddAIControllerItems()
-    {
+
+    static void AddAIControllerItems() {
         Selection.activeGameObject.AddComponent<GameVehicleAIWaypointEditor>();
         Selection.activeGameObject.AddComponent<GameVehicleRespawnController>();
         Selection.activeGameObject.AddComponent<GameVehicleAIDriverController>();
         Selection.activeGameObject.AddComponent<GameVehicleMotorMapping>();
-        Selection.activeGameObject.AddComponent<ShowControllerRaycasts>();            
-		
-		GameVehicleMotorMapping aIMotorMapping;
-		aIMotorMapping = Selection.activeGameObject.GetComponent("GameVehicleMotorMapping")as GameVehicleMotorMapping;
-		aIMotorMapping.usingGameVehicleAIDriverMotor = false;
-		
-        GameObject viewPoint = new GameObject();		
+        Selection.activeGameObject.AddComponent<ShowControllerRaycasts>();
+
+        GameVehicleMotorMapping aIMotorMapping;
+        aIMotorMapping = Selection.activeGameObject.GetComponent("GameVehicleMotorMapping") as GameVehicleMotorMapping;
+        aIMotorMapping.usingGameVehicleAIDriverMotor = false;
+
+        GameObject viewPoint = new GameObject();
         viewPoint.name = "ViewPoint";
         viewPoint.transform.parent = Selection.activeGameObject.transform;
         viewPoint.transform.localPosition = Vector3.zero;
         viewPoint.transform.localScale = new Vector3(1, 1, 1);
         viewPoint.transform.localRotation = Quaternion.identity;
         viewPoint.AddComponent<ViewPointBehaviour>();
-		Vector3 newViewPointPos;
-		newViewPointPos = viewPoint.transform.position;
-		newViewPointPos.y += 1;
-		viewPoint.transform.position = newViewPointPos;
-		
-        GameObject viewPointCollider = GameObject.CreatePrimitive(PrimitiveType.Cube);        
-        viewPointCollider.name="ViewPointCollider";		
+        Vector3 newViewPointPos;
+        newViewPointPos = viewPoint.transform.position;
+        newViewPointPos.y += 1;
+        viewPoint.transform.position = newViewPointPos;
+
+        GameObject viewPointCollider = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        viewPointCollider.name = "ViewPointCollider";
         viewPointCollider.transform.parent = Selection.activeGameObject.transform;
-		viewPointCollider.transform.localPosition = Vector3.zero;
-		viewPointCollider.transform.localScale = new Vector3(1, 1, 1);
+        viewPointCollider.transform.localPosition = Vector3.zero;
+        viewPointCollider.transform.localScale = new Vector3(1, 1, 1);
         viewPointCollider.transform.localRotation = Quaternion.identity;
         viewPointCollider.transform.GetComponent<Renderer>().enabled = false;
 
         GameVehicleAIDriverController aiDriverController;
         aiDriverController = Selection.activeGameObject.GetComponent("GameVehicleAIDriverController") as GameVehicleAIDriverController;
         aiDriverController.viewPoint = viewPoint.transform;
-        
-    }	
-	
-	[MenuItem("Component/AI/Component/Set Startposition")]
-    static void AddSetStartPosition()
-    {
+
+    }
+
+    [MenuItem("Component/AI/Component/Set Startposition")]
+    static void AddSetStartPosition() {
         Selection.activeGameObject.AddComponent<SetStartPosition>();
     }
-	
-	//2012-06-23 -B
-	/*
+
+    //2012-06-23 -B
+    /*
 	[MenuItem("Custom/AI Driver Toolkit/Component/Switch OA Mode")]
     static void AddSwitchOAMode()
     {
@@ -248,18 +230,15 @@ public class GameVehicleAIDriverMenu : GameObjectBehavior
         Selection.activeGameObject.AddComponent<SplinePathWaypoints>();
     }
     */
-	//2012-06-23 -E
-	
-	[MenuItem("Component/AI/Online Documentation")]
-    static void OpenWebDocumentation()
-    {
-		Process.Start("http://www.seifert-engineering.de/downloads/ai-driver-toolkit-documentation.pdf");        
+    //2012-06-23 -E
+
+    [MenuItem("Component/AI/Online Documentation")]
+    static void OpenWebDocumentation() {
+        Process.Start("http://www.seifert-engineering.de/downloads/ai-driver-toolkit-documentation.pdf");
     }
-	
-	[MenuItem("Component/AI/Tutorials and more")]	
-    static void OpenWebTutorials()
-    {		
+
+    [MenuItem("Component/AI/Tutorials and more")]
+    static void OpenWebTutorials() {
         Process.Start("http://www.seifert-engineering.de/ai-driver-toolkit/ai-driver-toolkit-tutorials/");
     }
-	
 }
